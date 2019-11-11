d75fcb181d02db6b167ce6f8371da537c79ab130
Jessie <42197855+yujielyu@users.noreply.github.com>
2019-11-11 17:12:09 +1100

    Initial project

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Yujie LYU
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+/**
+ * @author Yujie LYU
+ */
+public class MainTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+}
